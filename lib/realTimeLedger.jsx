ec1790ac7b1ed78e7f75108c24a04fb181c350a5
robadamyan <robert.adamyan1@gmail.com>
2016-11-10 14:51:06 +0400

    change tab names

--- lib/realTimeLedger.jsx
+++ lib/realTimeLedger.jsx
@@ -57,6 +57,7 @@ class RealTimeLedger extends Component {
           <li style={this.props.myStyle.list_items}>
             Hospital
           </li>
+          </li>
         </div>
         {TransactionFullList}
       </div>
